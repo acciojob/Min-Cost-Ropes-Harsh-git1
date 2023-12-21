27ff75e20627c0644ddcacd34360a5ac43e16ed8
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-21 19:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function mincost(arr)
 { 
+	
 //write your code here
+	let cost = 0;
+	arr.sort((a,b) => {
+		a-b;
+	});
+
+	while(arr.length > 1) {
+		let rope1 = arr.shift();
+		let rope2 = arr.shift();
+
+		cost += rope1+rope2;
+		arr.push(rope1+rope2);
+		arr.sort((a,b) => {
+			a-b;
+		});
+	}
 // return the min cost
-  
+	  return cost;
 }
 
 module.exports=mincost;
